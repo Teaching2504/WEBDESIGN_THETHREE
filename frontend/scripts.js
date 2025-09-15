6e6735ffd9d237b552a9dcd49b91bd60769d8414
Marcoh05P <hunghoang2k5.tk@outlook.com>
2025-09-15 21:46:34 +0700

    Update responsive design and fix bugs

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,3 +1,10 @@
 //Click vao doi logo
 const logo = document.getElementById('logo');
 let clicked = false;
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
